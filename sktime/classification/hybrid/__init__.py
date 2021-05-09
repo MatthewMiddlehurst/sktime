f8d308843acd3272d53efd92d7f202e9fe09a0bb
Matthew Middlehurst <pfm15hbu@uea.ac.uk>
2021-05-09 09:56:04 +0100

    c22pr formatting 1

--- sktime/classification/hybrid/__init__.py
+++ sktime/classification/hybrid/__init__.py
@@ -5,7 +5,8 @@ __all__ = [
     "HIVECOTEV2",
 ]
 
-from sktime.classification.hybrid._catch22_forest_classifier import \
-    Catch22ForestClassifier
+from sktime.classification.hybrid._catch22_forest_classifier import (
+    Catch22ForestClassifier,
+)
 from sktime.classification.hybrid._hivecote_v1 import HIVECOTEV1
 from sktime.classification.hybrid._hivecote_v2 import HIVECOTEV2
