9024c0bc836f7700508c4f2fff3c908f02f0f20c
Matthew Middlehurst <pfm15hbu@uea.ac.uk>
2021-05-18 13:59:30 +0100

    remove old cit file and folder

--- sktime/classification/interval_based/vector_classifiers/__init__.py
+++
@@ -1,7 +0,0 @@
-# -*- coding: utf-8 -*-
-__all__ = [
-    "ContinuousIntervalTree",
-]
-
-from sktime.classification.interval_based.vector_classifiers._continuous_interval_tree import \
-    ContinuousIntervalTree
